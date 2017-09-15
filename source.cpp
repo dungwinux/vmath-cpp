a042e20144281433d3147b459dca025ce4b415ba
DungWinux <ntddebugger@gmail.com>
2017-09-15 12:46:58 +0700

    Add Param read

--- source.cpp
+++ source.cpp
@@ -1,5 +1,9 @@
 #include "source.h"
 
-int main(){
+int main(int argc, char* argv[]){
 	cout << ProgramName << " " << VersionInfo << " (Build " << Build << ")" << endl << Copyright << endl;
+	cout << endl;
+	for (int i = 1; i < argc; i++){
+		cout << argv[i] << endl;
+	}
 }
